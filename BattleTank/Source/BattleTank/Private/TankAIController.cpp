ba03deb172853fef63e1bea54e1256c29b15897f
DESKTOP-TGMQ4S0\Rachel Bood <rachel.bood@gmail.com>
2018-03-10 13:44:32 -0400

    Worked on line tracing via the reticule on the player controller's camera to set up the tank shooting gameplay feature

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "tank.h"
+
+
+ATank* ATankAIController::GetControlledTank() const 
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank * ATankAIController::GetPlayerTank() const
+{
+	return Cast<ATank>(GetWorld()->GetFirstPlayerController()->GetPawn());
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	auto PlayerPawn = GetPlayerTank();
+
+	if (!PlayerPawn)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Could not find PlayerTank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerTank is: %s"), (*PlayerPawn->GetName()));
+	}
+
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController not possesing a tank"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController possessing: %s"), (*ControlledTank->GetName()));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("TankAIController Begin Play"));
+}
+
---
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tank.h"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+	
+private:
+	ATank* GetControlledTank() const;
+
+	// Start the tank moving the barrel so that a shot would 
+	// hit where the crosshair intersects the world
+	void AimTowardsCrosshair();
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+	bool GetLookDirection(FVector2D ScreenLocation, FVector & LookDirection) const;
+	bool GetLookVectorHitLocation(FVector& LookDirection, FVector& HitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.33333;
+
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000.0;
+};
