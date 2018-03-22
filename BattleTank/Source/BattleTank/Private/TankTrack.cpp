d911c7fb3e97da50593fbed3341c379ca800b5a7
DESKTOP-TGMQ4S0\Rachel Bood <rachel.bood@gmail.com>
2018-03-22 17:45:31 -0300

    Lecture 188: Avoiding Boolean Flags

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -5,7 +5,7 @@
 
 UTankTrack::UTankTrack()
 {
-    PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bCanEverTick = false;
     
 }
 
@@ -16,29 +16,41 @@ void UTankTrack::BeginPlay()
     OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
 }
 
-void UTankTrack::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction)
+
+void UTankTrack::ApplySidewaysForce()
 {
-    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+    auto DeltaTime = GetWorld()->GetDeltaSeconds();
 
     // Calculate the slippage speed
     auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
+    
     // Work-out the required acceleration this frame to correct
     auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
+    
     //Calculate and apply sideways force (F = m a)
     auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
     auto CorrectionForce = TankRoot->GetMass() * CorrectionAcceleration;
+
     TankRoot->AddForce(CorrectionForce);
 }
 
 void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* Otheromponent, FVector NormalImpulse, const FHitResult& Hit)
 {
+    DriveTrack();
+    ApplySidewaysForce();
 
+    // reset the throttle
+    CurrentThrottle = 0;
 }
 
 void UTankTrack::SetThrottle(float Throttle)
 {
-    Throttle = FMath::Clamp<float>(Throttle, -1, 1);
-    auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+    CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
+}
+
+void UTankTrack::DriveTrack()
+{
+    auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
     auto ForceLocation = GetComponentLocation();
     auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
     TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -29,8 +29,13 @@ public:
 private:
     UTankTrack();
     virtual void BeginPlay() override;
-    virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction) override;
+
+    void ApplySidewaysForce();
 
     UFUNCTION()
     void OnHit(UPrimitiveComponent * HitComponent, AActor * OtherActor, UPrimitiveComponent * Otheromponent, FVector NormalImpulse, const FHitResult & Hit);
+
+    void DriveTrack();
+    
+    float CurrentThrottle = 0;
 };
